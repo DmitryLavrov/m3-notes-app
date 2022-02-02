f2eb14818af880288b8bb48a849a72461d608f51
DmitryLavrov <d3236562@mail.ru>
2022-02-02 21:22:29 +0300

    L5 HW2 done. Note edit in list implemented

--- public/app.js
+++ public/app.js
@@ -6,16 +6,21 @@ addEventListener('click', async event => {
   }
 
   if (event.target.dataset.type === 'edit') {
-    const id = event.target.dataset.id
-    const title = event.target.dataset.title
-    const newTitle = prompt('Enter new title', title)
+    toggleEditModeOn(event.target.closest('li'))
+  }
 
-    if (newTitle === null) {
-      return
-    }
+  if (event.target.dataset.type === 'save') {
+    const id = event.target.dataset.id
+    const newTitle = event.target.closest('li').querySelector('input').value
 
     await edit(id, newTitle)
     event.target.closest('li').querySelector('span').innerText = newTitle
+
+    toggleEditModeOff(event.target.closest('li'))
+  }
+
+  if (event.target.dataset.type === 'cancel') {
+    toggleEditModeOff(event.target.closest('li'))
   }
 })
 
@@ -33,4 +38,23 @@ async function edit(id, title) {
       'Content-Type': 'application/json'
     }
   })
+}
+
+function toggleEditModeOn(nodeElement) {
+  nodeElement.querySelector('span').classList.add('d-none')
+  nodeElement.querySelector('input').classList.remove('d-none')
+  nodeElement.querySelector('button[data-type="edit"]').classList.add('d-none')
+  nodeElement.querySelector('button[data-type="remove"]').classList.add('d-none')
+  nodeElement.querySelector('button[data-type="save"]').classList.remove('d-none')
+  nodeElement.querySelector('button[data-type="cancel"]').classList.remove('d-none')
+  nodeElement.querySelector('input').focus()
+}
+
+function toggleEditModeOff(nodeElement) {
+  nodeElement.querySelector('span').classList.remove('d-none')
+  nodeElement.querySelector('input').classList.add('d-none')
+  nodeElement.querySelector('button[data-type="edit"]').classList.remove('d-none')
+  nodeElement.querySelector('button[data-type="remove"]').classList.remove('d-none')
+  nodeElement.querySelector('button[data-type="save"]').classList.add('d-none')
+  nodeElement.querySelector('button[data-type="cancel"]').classList.add('d-none')
 }
